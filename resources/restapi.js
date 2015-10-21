55c7a70cbe64a74f29f8359fa5b94291af4033ef
Alfred J Cepeda <AlfredJCepeda@gmail.com>
2015-10-21 09:53:49 -0700

    Adds some logs for debuggin

--- resources/restapi.js
+++ resources/restapi.js
@@ -18,19 +18,23 @@ var Restful = function(app) {
 			return res.sendStatus(400);
 		}
 
+		console.log("Valid compile request received");
 		var script = req.body.script;
 
 		var tmpFile = tmp.fileSync({ mode: 0644, postfix:".php", dir:"/var/tmp/eval/php" });
 		fs.writeSync(tmpFile.fd, script);
 
+		console.log("Creating docker command");
 		var name = path.basename(tmpFile.name);
 		var cmd = "sudo docker run --rm";
 		cmd += " --name "+name; 
 		cmd += " -v /var/tmp/eval/php:/script";
-		cmd += " -v /var/www/node/eval/resources/configs/php.ini:/usr/local/etc/php/php.ini"
+		cmd += " -v /var/www/node/eval/resources/configs/php.ini:/usr/local/etc/php/php.ini";
 		cmd += " php:5.6-cli php /script/" + name;
 
-		var result = shell.exec(cmd, function(error, stdout, stderr) {
+		console.log("Executing docker command");
+		shell.exec(cmd, function(error, stdout, stderr) {
+			console.log("Docker finished, sending result");
 			if(error) {
 				console.log("Docker: " + stderr);
 				res.sendStatus(500);
