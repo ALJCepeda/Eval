283089e5af4b1e81c3916b64580de63e5aee3420
Alfred J Cepeda <AlfredJCepeda@gmail.com>
2015-11-09 21:25:14 +0000

    Tests temper class for file/directory creation/deletion

--- resources/docker/temper.js
+++ resources/docker/temper.js
@@ -1,20 +1,24 @@
+var tmp = require("tmp");
+var path = require("path");
+var fs = require("fs");
+
 var Temper = function(root) {
 	this.root = root;
 	this.folders = [];
 	this.files = [];
 
 	this.cleanup = function() {
-		this.folders.forEach(function(dir) {
-			dir.removeCallback();
-		});
-
 		this.files.forEach(function(file) {
 			file.removeCallback();
 		});
 
+		this.folders.forEach(function(dir) {
+			dir.removeCallback();
+		});
+
 		this.folders = [];
 		this.files = [];
-	}
+	};
 
 	this.createCode = function(code, ext, folder) {
 		folder = folder || "";
@@ -23,17 +27,24 @@ var Temper = function(root) {
 		var file = this.createFile(dir.name);
 
 		fs.writeSync(file.fd, code);
+
+		return {
+			dir:dir,
+			file:file
+		};
 	};
 
 	this.createFolder = function(folder) {
-		var dir = tmp.dirSync({ mode:0744, template:path.join(tbis.root, folder, "XXXXXXX"), unsafeCleanup:true });
+		var dir = tmp.dirSync({ mode:0744, template:path.join(this.root, folder, "XXXXXXX"), unsafeCleanup:true });
 		this.folders.push(dir);
-		return tmpdir;
+		return dir;
 	};
 
 	this.createFile = function(dir, ext) {
-		var file = tmp.fileSync({ mode:0744, postfix:ext, dir:dir });
+		var file = tmp.fileSync({ mode:0744, postfix:"." + ext, dir:dir });
 		this.files.push(file);
 		return file;
-	}.
-}
+	};
+};
+
+module.exports = Temper;
--- resources/docker/tests/temper.js
+++ resources/docker/tests/temper.js
@@ -1,19 +1,73 @@
-var assert= require('assert');
-var should = require('should');
+var should = require("should");
+var path = require("path");
+var fs = require("fs");
 
-var Tempter = require('../temper.js');
-var descriptors = require('../descriptors.js');
-var Generator = require('../generator.js');
+var Temper = require("../temper.js");
 
-describe('Temper', function() {
-	describe('Path creation', function() {
+describe("Temper", function() {
+	describe("Path creation", function() {
 		var tmpDir = "/var/tmp/eval";
+		const output = "console.log('Hello World!');";
 
-		it('should create a folder', function() {
-			var temper = new Tempter(tmpDir);
-
-			var folder = tempter.createFolder('test');
+		it("should create a folder", function(done) {
+			var temper = new Temper(tmpDir);
+			var folder = temper.createFolder("test");
+			
+			should.exist(folder);
 			console.log(folder.name);
+
+			fs.stat(folder.name, function(err, stats) {
+				should.exist(stats);
+
+				temper.cleanup();
+
+				fs.stat(folder.name, function(err, stats) {
+					should.not.exist(stats);
+					done();
+				});
+			});
+		});
+
+		it("should create a file", function(done) {
+			var temper = new Temper(tmpDir);
+			var folder = temper.createFolder("test");
+			var file = temper.createFile(folder.name, "js");
+
+			should.exist(file);
+
+			var filename = path.basename(file.name);
+			var ext = filename.substring(filename.indexOf("."), filename.length);
+			(ext).should.equal(".js");
+
+			fs.stat(file.name, function(err, stats) {
+				should.exist(stats);
+
+				temper.cleanup();
+
+				fs.stat(file.name, function(err, stats) {
+					should.not.exist(stats);
+				});
+
+				fs.stat(folder.name, function(err, stats) {
+					should.not.exist(stats);
+					done();
+				});
+			});
+		});
+
+		it("should create a temporary file with code", function(done) {
+			var temper = new Temper(tmpDir);
+			var result = temper.createCode(output, "js", "test");
+
+			var file = result.file;
+			fs.readFile(file.name, "utf8", function(err, data) {
+				should.not.exist(err);
+				should.exist(data);
+
+				(data).should.equal(output);
+				temper.cleanup();
+				done();
+			});
 		});
 	});
 });
